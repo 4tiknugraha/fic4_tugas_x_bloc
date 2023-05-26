5ab30bc046d5fae7ed2263b11681f4595bbbb617
Atik <atik@192.168.31.96>
2023-05-26 14:28:21 +0700

    add file

---
+++ lib/bloc/register/register_bloc.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: depend_on_referenced_packages, avoid_print
+
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+import 'package:fic4_tugas_x_bloc/data/datasources/auth_datasources.dart';
+import 'package:fic4_tugas_x_bloc/data/models/request/register_model.dart';
+import 'package:fic4_tugas_x_bloc/data/models/response/register_response_model.dart';
+
+part 'register_event.dart';
+part 'register_state.dart';
+
+class RegisterBloc extends Bloc<RegisterEvent, RegisterState> {
+  final AuthDatasource datasource;
+  RegisterBloc(
+    this.datasource,
+  ) : super(RegisterInitial()) {
+    on<SaveRegisterEvent>((event, emit) async {
+      emit(RegisterLoading());
+      final result = await datasource.register(event.request);
+      print(result);
+      emit(RegisterLoaded(model: result));
+    });
+  }
+}
